30ba112487e7595665ec94d9b24f8aec6cd2ffb3
Jerry R Peirce <bigtime_1976@hotmail.com>
2015-08-11 12:21:50 -0700

    Initial commit

---
+++ Signature/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Signature
+    {
+    internal class Sincerely
+        {
+        private static void Main ( string [ ] args )
+            {
+            Console.WriteLine ( Signed ( "Jerry", 'R', "Peirce" ) );
+            }
+
+        private static string Signed ( string first, char mid, string last )
+            {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine ( "---" ).Append ( first ).Append ( " " );
+            sb.Append ( mid ).Append ( " " ).AppendLine ( last );
+            string whole = sb.ToString();
+            return whole;
+            }
+        }
+    }
