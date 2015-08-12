d74346d69e1f673418396e9535ac0bd509a9936e
Jerry R Peirce <bigtime_1976@hotmail.com>
2015-08-11 17:54:19 -0700

    Added Contact (Not really contact, don't call that) and try/catch.

--- Signature/Program.cs
+++ Signature/Program.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Text;
 
-namespace Signature
-    {
-    internal class Sincerely
-        {
-        private static void Main ( string [ ] args )
-            {
-            string[] signed = { "Jerry", "R", "Peirce" };
+namespace Signature {
 
-            StringBuilder sb = new StringBuilder().AppendLine ( "---" );
-            foreach ( string item in signed )
-                {
-                sb.Append ( item ).Append ( " " );
+    internal class Sincerely {
+
+        private static void Main(string[] args) {
+            string[] signed = new string[3] { "Jerry", "R", "Peirce" };
+            string[] contact = new string[2] { "notreal@nomail.com", "900.909.9900" };
+            // "Mean" Gene Hotline: Kids get your parents permission!
+            try {
+                StringBuilder sb = new StringBuilder().AppendLine("---");
+                foreach(string item in signed) { sb.Append(item).Append(" "); }
+                Console.WriteLine(sb.ToString());
+                Console.WriteLine(String.Join(" | ", contact));
+                } catch(Exception ex) {
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
                 }
-            Console.WriteLine ( sb.ToString ( ) );
             }
         }
     }
