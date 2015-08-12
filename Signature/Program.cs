b04761be0bae99f99e17d60367b1dc6d5662488a
Jerry R Peirce <bigtime_1976@hotmail.com>
2015-08-11 18:35:29 -0700

    Fixed formatting.

--- Signature/Program.cs
+++ Signature/Program.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using System.Text;
 
-namespace Signature {
-
-    internal class Sincerely {
-
-        private static void Main(string[] args) {
-            string[] signed = new string[3] { "Jerry", "R", "Peirce" };
-            string[] contact = new string[2] { "notreal@nomail.com", "900.909.9900" };
-            // "Mean" Gene Hotline: Kids get your parents permission!
-            try {
-                StringBuilder sb = new StringBuilder().AppendLine("---");
-                foreach(string item in signed) { sb.Append(item).Append(" "); }
-                sb.AppendLine().AppendLine(String.Join(" | ", contact));
-                Console.WriteLine(sb.ToString());
-                } catch(Exception ex) {
-                Console.WriteLine(ex.GetType().Name);
-                Console.WriteLine(ex.Message);
-                }
-            }
+namespace Signature
+{
+  internal class Sincerely
+  {
+    private static void Main(string[] args)
+    {
+      string[] signed = new string[3] { "Jerry", "R", "Peirce" };
+      string[] contact = new string[2] { "notreal@nomail.com", "900.909.9900" };
+      // "Mean" Gene Hotline: Kids get your parents permission!
+      try
+      {
+        StringBuilder sb = new StringBuilder().AppendLine("---");
+        foreach (string item in signed)
+        {
+          sb.Append(item).Append(" ");
         }
+        sb.AppendLine().AppendLine(String.Join(" | ", contact));
+        Console.WriteLine(sb.ToString());
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.GetType().Name);
+        Console.WriteLine(ex.Message);
+      }
     }
+  }
+}
